66ffddaf16fd455c3886937c2471f4dfa5560f26
Idmar Ramos Junior (Id) <idmar.ramos@booking.com>
2017-10-28 17:01:56 +0200

    Add autoprefixer and sourcemaps to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('./scss/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer({ browsers: ['last 3 versions'] }))
         .pipe(gulp.dest('./dist/css'))
         .pipe(minifyCSS())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/css/minified'))
 });
 
