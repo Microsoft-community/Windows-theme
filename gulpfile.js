c928ee2a8f3e5a50a927e67c55f6f3227c87978d
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2017-10-28 20:22:37 +0200

    Add autoprefixer to gulpfile.js (#193)
    * Add autoprefixer and sourcemaps to gulpfile
    
    * Add missed .pipe(sourcemaps.init()) to gulpfile.js
    
    * Compiled files with autoprefixer and sourcemaps
    
    * Remove sourcemap after discussion in #193
    
    Sourcemaps arent exactly usefull due to how reddit handles custom CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('./scss/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer({ browsers: ['last 3 versions'] }))
         .pipe(gulp.dest('./dist/css'))
         .pipe(minifyCSS())
         .pipe(gulp.dest('./dist/css/minified'))
