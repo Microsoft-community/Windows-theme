32640a45957f2a73975744af3d7148d00671cdb6
Idmar Ramos Junior (Id) <idmar.ramos@booking.com>
2017-10-28 20:19:20 +0200

    Remove sourcemap after discussion in #193
    Sourcemaps arent exactly usefull due to how reddit handles custom CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
-var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('./scss/*.scss')
-        .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer({ browsers: ['last 3 versions'] }))
         .pipe(gulp.dest('./dist/css'))
         .pipe(minifyCSS())
-        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/css/minified'))
 });
 
